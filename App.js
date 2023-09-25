314a5e802ac7949dc2760d6a740c78cafee4cf27
Magn8011 <91495188+Magn8011@users.noreply.github.com>
2023-09-25 21:56:51 +0200

    Endeligt commit

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getApps, initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeqQluJe8Wz37Q9iwdRE9ky7pu0DrkoQ0",
+  authDomain: "godkendelseopgave1.firebaseapp.com",
+  projectId: "godkendelseopgave1",
+  storageBucket: "godkendelseopgave1.appspot.com",
+  messagingSenderId: "783482051352",
+  appId: "1:783482051352:web:a5a18ba1782418ab429c6d"
+};
+
 
 export default function App() {
   return (
